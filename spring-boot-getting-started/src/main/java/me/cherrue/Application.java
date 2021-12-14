5c34194a259c1195c4f1f8b962da77f568ae0dd2
cherrue <th885172@gmail.com>
2021-12-14 18:02:32 +0900

    - [feature] set application to none web application

--- spring-boot-getting-started/src/main/java/me/cherrue/Application.java
+++ spring-boot-getting-started/src/main/java/me/cherrue/Application.java
@@ -1,11 +1,14 @@
 package me.cherrue;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
         SpringApplication.run(Application.class, args);
     }
 }
