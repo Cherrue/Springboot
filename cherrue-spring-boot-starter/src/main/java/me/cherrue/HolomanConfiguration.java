8c884677cfc7886d4eac58eb7efbcf5c19db8377
Cherrue <th885172@gmail.com>
2022-01-24 23:39:46 +0900

    [inflearn] 22-01-24 3부 스프링 부트 원리 의존성 관리 이해, 의존성 관리 응용, 자동 설정 이해, 자동 설정 만들기 1부 : Starter와 AutoConfigure

---
+++ cherrue-spring-boot-starter/src/main/java/me/cherrue/HolomanConfiguration.java
@@ -0,0 +1,15 @@
+package me.cherrue;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HolomanConfiguration {
+    @Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(5);
+        holoman.setName("Cherrue");
+        return holoman;
+    }
+}
