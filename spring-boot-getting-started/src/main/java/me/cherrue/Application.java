294f9d2f3468dedba565c77d87861e5e6ebe9f08
Cherrue <th885172@gmail.com>
2022-01-27 23:30:13 +0900

    [Inflear] 스프링 부트 개념과 활용 - 3부 스프링 부트 원리 > 내장 웹 서버 이해 tomcat 띄우기 실습

--- spring-boot-getting-started/src/main/java/me/cherrue/Application.java
+++ spring-boot-getting-started/src/main/java/me/cherrue/Application.java
@@ -1,14 +1,41 @@
 package me.cherrue;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context context = tomcat.addContext("", "/");
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>aaa</title></head>");
+                writer.println("<body><h1>Hello Tomcat!</h1></body>");
+            }
+        };
+        String servletName = "helloServlet";
+        tomcat.addServlet("", servletName, servlet);
+        context.addServletMappingDecoded("/hello", servletName);
+
+        tomcat.start();
+        tomcat.getServer().await(); // 요청을 기다린다
     }
 }
