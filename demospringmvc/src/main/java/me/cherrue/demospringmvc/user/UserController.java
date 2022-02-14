fe07ff2dfecc0d2b66726a861fd201285ba5dc4e
Cherrue <th885172@gmail.com>
2022-02-14 23:32:02 +0900

    [Inflearn] 스프링 부트 개념과 활용 - 4부 스프링 부트 활용 > 스프링 웹 MVC 2부 HttpMessageConverter HttpMessageConverter가 호출되는 기본 스프링 웹 MVC 코드 작성(RestController)

--- demospringmvc/src/main/java/me/cherrue/demospringmvc/user/UserController.java
+++ demospringmvc/src/main/java/me/cherrue/demospringmvc/user/UserController.java
@@ -1,12 +1,17 @@
 package me.cherrue.demospringmvc.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    //public @ResponseBody User Create(@RequestBody User user) {
+    @PostMapping("/users/create")
+    public /*@ResponseBody*/ User Create(@RequestBody User user) {
+        return user;
     }
 }
