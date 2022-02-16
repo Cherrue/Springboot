352f77c0d891ccd73e5990963cba7decd6b4596f
Cherrue <th885172@gmail.com>
2022-02-16 22:34:37 +0900

    [Inflearn] 스프링 부트 개념과 활용 - 4부 스프링 부트 활용 > 스프링 웹 MVC 4부 - 정적 리소스 서비스 - web jar 로 jquery 쓰기 - webjars-locator-core 쓰기 - index 페이지 만들기 - 파비콘 붙이기

--- demospringmvc/src/main/java/me/cherrue/demospringmvc/config/WebConfig.java
+++ demospringmvc/src/main/java/me/cherrue/demospringmvc/config/WebConfig.java
@@ -2,10 +2,18 @@ package me.cherrue.demospringmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc : 이걸 붙이면 기본 설정이 다 없어짐
 public class WebConfig implements WebMvcConfigurer {
 //    void configureAsyncSupport(AsyncSupportConfigurer configurer) {}
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("m/**")
+                .addResourceLocations("classpath:/m/") // 반드시 /로 끝내야 매핑이 잘 된다.
+                .setCachePeriod(20);
+    }
 }
