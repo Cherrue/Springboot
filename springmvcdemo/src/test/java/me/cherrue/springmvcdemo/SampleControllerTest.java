84b422c9e27a67e06a4d28712d132bf98ff9d3d3
Cherrue <th885172@gmail.com>
2022-02-18 16:38:11 +0900

    [Inflearn] 스프링 부트 개념과 활용 - 4부 스프링 부트 활용 > 스프링 웹 MVC - thymeleaf 를 이용한 view 서비스 - HtmlUnit을 이용한 HTML 테스트

---
+++ springmvcdemo/src/test/java/me/cherrue/springmvcdemo/SampleControllerTest.java
@@ -0,0 +1,52 @@
+package me.cherrue.springmvcdemo;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    WebClient webClient;
+
+    @Test
+    public void helloHtmlUnit() throws IOException {
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("cherrue");
+    }
+
+    @Test
+    public void hello() throws Exception {
+        // 요청 : "/hello"
+        // 응답
+        // - 모델 name : cherrue
+        // - 뷰 이름 : hello
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("cherrue")))
+                .andExpect(content().string(containsString("cherrue")));
+    }
+}
