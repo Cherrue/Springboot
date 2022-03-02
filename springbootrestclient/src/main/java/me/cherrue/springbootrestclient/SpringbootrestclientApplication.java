7179bb8f0b542886f9e36f8d7f614fd02d0f723f
Cherrue <th885172@gmail.com>
2022-03-02 20:01:41 +0900

    [Inflearn] 스프링 부트 개념과 활용 - 4부 스프링 부트 활용 > 스프링 REST 클라이언트 - RestTemplate 과 WebClient 비교 runner 작성 - builder 글로벌 커스텀 빈 등록

---
+++ springbootrestclient/src/main/java/me/cherrue/springbootrestclient/SpringbootrestclientApplication.java
@@ -0,0 +1,26 @@
+package me.cherrue.springbootrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SpringbootrestclientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootrestclientApplication.class, args);
+    }
+
+    @Bean
+    public WebClientCustomizer webClientCustomizer() {
+        return webClientBuilder -> webClientBuilder.baseUrl("http://localhost:8080");
+    }
+
+    @Bean
+    public RestTemplateCustomizer restTemplateCustomizer() {
+        return restTemplate -> restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+    }
+}
