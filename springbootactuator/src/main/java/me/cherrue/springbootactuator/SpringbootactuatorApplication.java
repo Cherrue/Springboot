9420925c887c67c6aefffab85f32482d72911116
Cherrue <th885172@gmail.com>
2022-03-04 00:18:47 +0900

    [Inflearn] 2022-03-04 스프링 부트 운영 > 스프링 Actuator 빼먹은 파일 추가 inflearn > 스프링 부트 개념과 활용 > 5부 스프링 부트 운영 - actuator 사용하기 - jconsole, jvisualvm 사용하기 - spring-boot-admin-starter 사용하기
    inflearn > 스프링 부트 개념과 활용 > 6부 정리

--- springbootactuator/src/main/java/me/cherrue/springbootactuator/SpringbootactuatorApplication.java
+++ springbootactuator/src/main/java/me/cherrue/springbootactuator/SpringbootactuatorApplication.java
@@ -1,2 +1,18 @@
-package me.cherrue.springbootactuator;public class SpringbootactuatorApplication {
+package me.cherrue.springbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootactuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootactuatorApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
--- springbootmonitor/src/main/java/me/cherrue/springbootmonitor/SpringbootmonitorApplication.java
+++ springbootmonitor/src/main/java/me/cherrue/springbootmonitor/SpringbootmonitorApplication.java
@@ -1,2 +1,13 @@
-package me.cherrue.springbootmonitor;public class SpringbootmonitorApplication {
+package me.cherrue.springbootmonitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringbootmonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmonitorApplication.class, args);
+    }
 }
